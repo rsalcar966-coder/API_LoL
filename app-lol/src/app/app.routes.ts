5e098927cda4f09fe78b7857b3a35318800b6b65
rsalcar966-coder <rsalcar966@gmail.com>
2026-05-28 10:14:10 +0200

    Update app.routes.ts

--- app-lol/src/app/app.routes.ts
+++ app-lol/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Contact } from '../components/contact/contact';
+import { Home } from '../components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "contact", component: Contact },
+    { path: "", component: Home }
+];
